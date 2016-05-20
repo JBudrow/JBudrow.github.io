64e89a6019173a0016aa226756af99a6aa9ed663
jbudrow <jbudrowd3@yahoo.com>
2016-05-20 15:30:18 -0400

    try different side nav variation and jquery pt2

--- js/init.js
+++ js/init.js
@@ -1,8 +1,1 @@
-(function($){
-  $(function(){
-
-    $('.button-collapse').sideNav('show');
-    $('.button-collapse').sideNav('hide');
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+$(".button-collapse").sideNav();
