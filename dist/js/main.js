db9695c2d0b7a0b2d08f4d9dc67535c6877b47e2
jbudrow <jbudrowd3@yahoo.com>
2016-05-26 13:43:42 -0400

    is the nav js overriding modal function? testing 3

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,14 +1,10 @@
-$(document).ready(function() {
-    $('div').fadeIn('slow');
-    $('.pop').on('click', function() {
-      $('.imagepreview').attr('src', $(this).find('img').attr('src'));
-      $('#imagemodal').modal('show');
-    });
-});
-
-// $(function() {
-// 		$('.pop').on('click', function() {
-// 			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
-// 			$('#imagemodal').modal('show');
-// 		});
+// $(document).ready(function() {
+//     $('div').fadeIn('slow');
 // });
+
+$(function() {
+		$('.pop').on('click', function() {
+			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+			$('#imagemodal').modal('show');
+		});
+});
