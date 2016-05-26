1b85cdd1e60a4fc48172d66849c186a0f6c72a8a
jbudrow <jbudrowd3@yahoo.com>
2016-05-26 13:41:40 -0400

    change favicon, tweak main js, testing 2

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,10 +1,14 @@
 $(document).ready(function() {
     $('div').fadeIn('slow');
+    $('.pop').on('click', function() {
+      $('.imagepreview').attr('src', $(this).find('img').attr('src'));
+      $('#imagemodal').modal('show');
+    });
 });
 
-$(function() {
-		$('.pop').on('click', function() {
-			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
-			$('#imagemodal').modal('show');
-		});
-});
+// $(function() {
+// 		$('.pop').on('click', function() {
+// 			$('.imagepreview').attr('src', $(this).find('img').attr('src'));
+// 			$('#imagemodal').modal('show');
+// 		});
+// });
