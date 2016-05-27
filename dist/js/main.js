e431aaa8423834343cb47804bd079ee6c440e963
jbudrow <jbudrowd3@yahoo.com>
2016-05-27 15:21:39 -0400

    implement js function for dyanmic web scrolling, column reconstruction

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,6 +1,21 @@
 // $(document).ready(function() {
-//     $('div').fadeIn('slow');
+//     $('.container').fadeIn('slow');
 // });
+
+$('.navbar-default .navbar-nav>li').click(function(e){
+	var currentContainer = '#' + $('.current').attr('id');
+	var clickedContainer = $(this).children(":first").attr('href');
+	if(typeof clickedContainer === 'undefined') {
+		clickedContainer = $(this).attr('href');
+	}
+	if (currentContainer != clickedContainer) {
+		$('div.current').fadeOut(300, function(){
+			$(clickedContainer).fadeIn(300)});
+		$('.current').removeClass('current');
+		$(clickedContainer).addClass('current');
+	}
+	e.preventDefault();
+});
 
 $(function() {
 		$('.pop').on('click', function() {
