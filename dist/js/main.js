4b8aaf7ffbd715c7dbc6da1de4602d585a5fd17b
jbudrow <jbudrowd3@yahoo.com>
2016-05-31 22:50:50 -0400

    add shell blinking cursor and script, fine tuning and styling

--- dist/js/main.js
+++ dist/js/main.js
@@ -23,3 +23,15 @@ $(function() {
 			$('#imagemodal').modal('show');
 		});
 });
+
+$(document).ready(function() {
+    setInterval ('cursorAnimation()', 600);
+});
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
